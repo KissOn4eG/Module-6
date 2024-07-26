de4c5ca4d19daf04e29cb0aa9ce9cad9a9c85388
KissOn4eG <144478337+KissOn4eG@users.noreply.github.com>
2024-07-26 17:31:07 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,42 @@
+class Horse:
+    x_distance = 0
+    def __init__(self, sound='Frrr'):
+        self.sound = sound
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+        return dx
+
+
+class Eagle:
+    y_distance = 0
+    def __init__(self, sound='I train, eat, sleep, and repeat'):
+        self.sound = sound
+
+    def fly(self, dy):
+        self.y_distance += dy
+        return dy
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
